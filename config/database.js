9aab8058c042669b6b6b800a2a9a2262b71a072d
mndhlovu <40129820+mrndhlovu@users.noreply.github.com>
2022-05-22 16:40:47 +0100

    add database options

--- config/database.js
+++ config/database.js
@@ -1,15 +1,41 @@
-const path = require('path');
-
-module.exports = ({env}) => ({
+const sqlite = {
+  client: 'sqlite',
   connection: {
-    client: 'sqlite',
-    connection: {
-      filename: path.join(
-        __dirname,
-        '..',
-        env('DATABASE_FILENAME', '.tmp/data.db')
-      ),
-    },
-    useNullAsDefault: true,
+    filename: '.tmp/data.db',
   },
-});
+  useNullAsDefault: true,
+};
+
+const postgres = {
+  client: 'postgres',
+  connection: {
+    host: process.env.DATABASE_HOST,
+    port: 5432,
+    database: process.env.DATABASE_NAME,
+    user: process.env.DATABASE_NAME,
+    password: process.env.DATABASE_PASSWORD,
+  },
+};
+
+const mysql = {
+  client: 'mysql',
+  connection: {
+    database: 'strapi',
+    user: 'strapi',
+    password: 'strapi',
+    port: 3306,
+    host: 'localhost',
+  },
+};
+
+const db = {
+  mysql,
+  sqlite,
+  postgres,
+};
+
+module.exports = {
+  connection: process.env.DATABASE
+    ? db[process.env.DATABASE] || db.sqlite
+    : db.sqlite,
+};
