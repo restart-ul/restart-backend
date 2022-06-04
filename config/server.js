299a2ed87240749fdf86c3fd2eee9900e5f3bdd5
mndhlovu <mndhlovu@hubspot.com>
2022-06-04 20:15:10 +0100

    debug database

--- config/server.js
+++ config/server.js
@@ -1,8 +1,10 @@
 module.exports = ({env}) => {
+  console.log('env', env('STRAPI_ENDPOINT', 'http://127.0.0.1:1337'));
+
   return {
     host: env('HOST', '0.0.0.0'),
     port: env.int('PORT', 1337),
-    url: env('STRAPI_ENDPOINT', 'http://localhost:1337/'),
+    url: env('STRAPI_ENDPOINT', 'http://127.0.0.1:1337'),
     app: {
       keys: env.array('APP_KEYS', ['testKey1', 'testKey2']),
     },
