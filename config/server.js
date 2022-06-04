672b8ffd22c320fae23ceab2a2e8fe7a0f042b3a
mndhlovu <mndhlovu@hubspot.com>
2022-06-04 02:07:09 +0100

    test pm2

--- config/server.js
+++ config/server.js
@@ -1,8 +1,12 @@
-module.exports = ({env}) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  url: env('STRAPI_ENDPOINT','http://localhost:1337/'),
-  app: {
-    keys: env.array('APP_KEYS', ['testKey1', 'testKey2']),
-  },
-});
+module.exports = ({env}) => {
+  console.log({env: env('STRAPI_ENDPOINT')});
+
+  return {
+    host: env('HOST', '0.0.0.0'),
+    port: env.int('PORT', 1337),
+    url: env('STRAPI_ENDPOINT', 'http://localhost:1337/'),
+    app: {
+      keys: env.array('APP_KEYS', ['testKey1', 'testKey2']),
+    },
+  };
+};
