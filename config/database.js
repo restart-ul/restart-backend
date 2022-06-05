75863d7bf602d2a8adf74cff600e5256e79ad843
mndhlovu <mndhlovu@hubspot.com>
2022-06-05 07:20:22 +0100

    debug database

--- config/database.js
+++ config/database.js
@@ -12,7 +12,7 @@ const postgres = {
     host: process.env.DATABASE_HOST,
     port: 5432,
     database: process.env.DATABASE_NAME,
-    user: process.env.DATABASE_NAME,
+    user: process.env.DATABASE_USERNAME,
     password: process.env.DATABASE_PASSWORD,
   },
 };
